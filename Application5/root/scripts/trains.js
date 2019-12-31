09a2b7869aef95e9be5a4f62167b263eb945b41d
Jason Corvell Rice <ricejason69@gmail.com>
2019-12-31 08:36:22 -0600

    Added resume and new learning module-project

---
+++ Application5/root/scripts/trains.js
@@ -0,0 +1,13 @@
+let totalTrains = 12;
+let trainsOperational = 8;
+
+console.log("Out of the "+totalTrains+ " total trains, "
+	+trainsOperational+" trains are running.");
+console.log("Train #"+1+" is running.");
+console.log("Train #"+2+" is running.");
+console.log("Train #"+3+" is running.");
+console.log("Train #"+4+" is running.");
+console.log("Train #"+5+" is running.");
+console.log("Train #"+6+" is running.");
+console.log("Train #"+7+" is running.");
+console.log("Train #"+8+" is running.");
